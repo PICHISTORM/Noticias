83663dd661707e50bb8478c005081a31bdb117b4
PICHISTORM <davidalejandrot522@gmail.com>
2026-06-25 20:05:23 -0600

    Mostrar articulos de noticia

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonCardSubtitle, IonCard, IonCardTitle, IonImg, IonCardContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
 import { NewsService } from '../../services/news.service';
-import { NewsResponse } from 'src/app/interfaces';
+import { Article, NewsResponse } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonGrid, IonRow, IonCol, IonCardSubtitle, IonCard, IonCardTitle, IonImg, IonCardContent],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+
+  public articles: Article[]= [];
+
   constructor(private newsService: NewsService) {}
 
 
@@ -18,7 +22,7 @@ ngOnInit() {
   this.newsService.gettopHeadlines()
     .subscribe({
       next: (articles) => {
-        console.log(articles);
+        this.articles.push(...articles);
       },
 
     });
